7480e740ee46a8633009ea136df225d47a5f4f2a
wanderramirezdiaz-ops <wanderramirezdiaz@gmail.com>
2026-07-19 22:08:38 +0000

    day 3 modificado con algunos algumentos

--- hackerrank/day2/main.py
+++ hackerrank/day2/main.py
@@ -1,35 +1,17 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
 
-#
-# Complete the 'solve' function below.
-#
-# The function accepts following parameters:
-#  1. DOUBLE meal_cost
-#  2. INTEGER tip_percent
-#  3. INTEGER tax_percent
-#
-
-def solve(meal_cost, tip_percent, tax_percent):
-    # Write your code here
-    comida = meal_cost
-    propina = meal_cost*(tip_percent/100)
-    impuesto = meal_cost*(tax_percent/100)
-    
-    total = comida + propina + impuesto
-    
-    print(f'El monto a pagar en caja es: {round(total)}')    
-
 if __name__ == '__main__':
-    meal_cost = float(input("Digite el monto bruto de la comida (decimal): ").strip())
+    N = int(input('Digite un mumero entero:').strip())
 
-    tip_percent = int(input("Digite el porcentaje de la propina (entero): ").strip())
-
-    tax_percent = int(input("Digite el porcentaje del impuesto (entero): ").strip())    
-
-    solve(meal_cost, tip_percent, tax_percent)
+    if N % 2 != 0:
+        print("Estoy feliz ")
+    elif N % 2 == 0 and 2 <= N <= 5:
+        print("Dios es bueno")
+    elif N % 2 == 0 and 6 <= N <= 20:
+        print("Dios bendice")
+    elif N % 2 == 0 and N > 20:
+        print("NO BULTO")
