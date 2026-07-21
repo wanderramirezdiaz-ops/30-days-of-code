6385bb20eb38a52fbdd1dd726f894ef6476d623f
wanderramirezdiaz-ops <wanderramirezdiaz@gmail.com>
2026-07-21 11:33:49 +0000

    dia 5 termiando con python

--- hackerrank/day2/day4/main.py
+++ hackerrank/day2/day4/main.py
@@ -24,3 +24,8 @@ t = int(input())
 for i in range(0, t):
     age = int(input())         
     p = Person(age)  
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()       
+    p.amIOld()
+    print("")
